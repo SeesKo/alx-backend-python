2270e23bad3af75d9a25b58c51f6c1b5de74efb3
SeesKo <seeskosee@seeskome.com>
2024-07-04 05:43:05 -0700

    annotate parameters

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module provides a type-annotated function to
+return tuples of elements and their lengths.
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples containing elements and their lengths.
+    """
+    return [(i, len(i)) for i in lst]
