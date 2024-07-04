8db0a0d124e69a80b3adb796caec79f8fca964a5
SeesKo <seeskosee@seeskome.com>
2024-07-04 06:10:01 -0700

    duck-typed annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -4,10 +4,10 @@ Module provides a duck-typed function to safely
 return the first element of a sequence.
 """
 
-from typing import Sequence, Union, Any
+from typing import Sequence, Union, Any, Optional
 
 
-def safe_first_element(lst: Sequence) -> Union[Any, None]:
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
     Returns the first element of the sequence if it exists,
     otherwise returns None.
