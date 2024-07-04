025c702ebbe0e2bd7a278f851a782be1cc7cb6c7
SeesKo <seeskosee@seeskome.com>
2024-07-04 04:30:57 -0700

    type-annotated add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module provides a type-annotated function to add two floats.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two floats and returns the result.
+    """
+    return a + b
