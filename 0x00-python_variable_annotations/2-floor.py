06b7399504812fb8001a2e0e8d813303c60110cf
SeesKo <seeskosee@seeskome.com>
2024-07-04 04:57:58 -0700

    type-annotated floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module provides a type-annotated function to return the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+    """
+    return math.floor(n)
