6ed2eae052286aac55cc0da9249224a9cb3d84ae
SeesKo <seeskosee@seeskome.com>
2024-07-08 02:19:37 -0700

    asynchronous co-routine

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module taking in integer argument
+and eventually returning it.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Wait for a random delay between 0 and max_delay
+    seconds and return the delay.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
