b9fba09f2cccb37dad60d09f1b9742b01013381a
SeesKo <seeskosee@seeskome.com>
2024-07-09 07:09:58 -0700

    return random numbers

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -5,12 +5,13 @@ async_comprehension four times in parallel using asyncio.gather.
 """
 
 import asyncio
+import time
 from typing import List
 from time import perf_counter
-from importlib import import_module as using
+from importlib import import_module as async_import
 
 
-async_comprehension = using('1-async_comprehension').async_comprehension
+async_comprehension = async_import('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
@@ -19,14 +20,6 @@ async def measure_runtime() -> float:
     four times in parallel.
     """
     start_time = perf_counter()
-
-    # Execute async_comprehension four times in parallel using asyncio.gather
-    await asyncio.gather(
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension(),
-        async_comprehension()
-    )
-
-    end_time = perf_counter()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end_time = time.perf_counter()
     return end_time - start_time
