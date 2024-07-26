afc417494ffaccfcbedd19cc103fbe6401f9902a
SeesKo <seeskosee@seeskome.com>
2024-07-26 05:29:33 -0700

    unit tests for github org client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -93,16 +93,16 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False)
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ({}, "my_license", False),
+        ({"license": {}}, "my_license", False),
     ])
     def test_has_license(self, repo, license_key, expected):
         """
-        Test the has_license method of GithubOrgClient
+        Test the has_license static method of GithubOrgClient.
         """
-        self.assertEqual(
-            GithubOrgClient.has_license(repo, license_key),
-            expected
-        )
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
 
 
 if __name__ == "__main__":
